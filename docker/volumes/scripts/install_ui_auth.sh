0b216b91da927b3ab652392c0d557cc57411cd60
tomato <tomatono@gmail.com>
2022-02-13 01:39:30 +0900

    ログインを有効にするシェルを追加

--- docker/volumes/scripts/install_ui_auth.sh
+++ docker/volumes/scripts/install_ui_auth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-composer require laravel/ui
-php artisan ui vue --auth
-npm install && npm run dev
-php artisan migrate
+cd /var/www/html \
+  && composer require laravel/ui \
+  && php artisan ui vue --auth \
+  && npm install && npm run dev \
+  && php artisan migrate

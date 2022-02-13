ecad2f38e39ccfbcf18bd2dd908f21c6d744d095
tomato <tomatono@gmail.com>
2022-02-13 14:55:22 +0900

    スクリプト修正

--- scripts/sail.sh
+++ scripts/sail.sh
@@ -2,7 +2,9 @@
 
 PWD=`pwd`
 cd ../docker/laravel_web
-sudo ./vendor/bin/sail "$@"
+sudo ../volumes/vendor/bin/sail "$@"
+# cd ../docker/laravel_web
+# sudo ./vendor/bin/sail "$@"
 # cd ../docker
 # sudo ./laravel_web/vendor/bin/sail $1
 cd $PWD

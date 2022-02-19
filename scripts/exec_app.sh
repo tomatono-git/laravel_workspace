3fa4f5e10b8389f553854ddd1867337c0cc48a53
tomato <tomatono@gmail.com>
2022-02-20 04:31:17 +0900

    スクリプト修正

--- scripts/exec_app.sh
+++ scripts/exec_app.sh
@@ -2,5 +2,5 @@
 
 PWD=`pwd`
 cd ../docker/laravel_web/
-./vendor/bin/sail exec -u sail -w /home/sail/scripts laravel.test "$@"
+../volumes/vendor/bin/sail exec -u sail -w /home/sail/scripts laravel.test "$@"
 cd $PWD

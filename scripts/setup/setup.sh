23f2cf0e0b33f4cb9c8785f448ac535f1420c12f
tomato <tomatono@gmail.com>
2022-02-20 05:10:15 +0900

    コメント削除

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -31,25 +31,3 @@ sudo chown -R $USER: $LARAVEL_WEB
 
 cd $LARAVEL_WORKSPACE/scripts
 ./sail.sh up
-
-
-
-
-# ### git clone 後に実行するスクリプト ###
-
-# # TODO ホストにPHPがないと動かない
-
-# # プロジェクト名
-# PROJECT_NAME=$1
-# if [ "$PROJECT_NAME" = "" ]; then
-#     PROJECT_NAME="laravel_web"
-# fi
-
-# # セットアップ
-# cd ../../docker/$PROJECT_NAME \
-#     && php ./artisan sail:install --with=pgsql,redis,meilisearch,mailhog,selenium --devcontainer \
-#     && chown -R $USER .
-
-# WHITE='\033[1;37m'
-# NC='\033[0m'
-# echo -e "${WHITE}Get started with:${NC} cd $PROJECT_NAME && ./vendor/bin/sail up"

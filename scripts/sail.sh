bf58fc775089c0a35336974c3eb32aaab9a97265
tomato <tomatono@gmail.com>
2022-02-19 16:16:32 +0900

    .env がない場合にコピー

--- scripts/sail.sh
+++ scripts/sail.sh
@@ -2,9 +2,18 @@
 
 PWD=`pwd`
 cd ../docker/laravel_web
+
+if [ ! -e .env ]; then
+    # laravel_web フォルダに .env ファイルがなければコピー。
+    cp ../volumes/env/psql.env .env
+fi
+
+# sail 実行
 sudo ../volumes/vendor/bin/sail "$@"
+
 # cd ../docker/laravel_web
 # sudo ./vendor/bin/sail "$@"
 # cd ../docker
 # sudo ./laravel_web/vendor/bin/sail $1
+
 cd $PWD
